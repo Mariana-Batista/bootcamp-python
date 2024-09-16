582eb4055ec82e6fe9f2fd92c3e1bfc293962f01
Mariana-Batista <marianabatista.eng@gmail.com>
2024-09-16 13:10:31 -0300

    commit exercicios.py

--- exercicios2.py
+++ exercicios2.py
@@ -1,10 +1,20 @@
 #Inteiros (int)
 #Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-numero1 = int(input("Digite um nº inteiro: "))
+
+"""numero1 = int(input("Digite um nº inteiro: "))
 numero2 = int(input("Digite um nº inteiro: "))
-print(numero1 + numero2)
+print(numero1 + numero2)"""
 
 #Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+"""numero = int(input("Digite um nº inteiro: "))
+print(numero%5)
+"""
 #Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+"""numero1 = int(input("Digite um nº inteiro: "))
+numero2 = int(input("Digite um nº inteiro: "))
+print(numero1 * numero2)"""
+
 #Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 #Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
